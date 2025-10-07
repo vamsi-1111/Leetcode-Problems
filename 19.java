4a31c9e0795a5b6fb8396b19aff9ec99e148ff7c
Vamsi Chitturi <vch2ri@gmail.com>
2025-10-07 19:45:23 -0400

    Add files via upload

---
+++ 19.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode ptr1 = dummy;
+        ListNode ptr2 = dummy;
+        int difference = n;
+
+        while(difference > 0) {
+            ptr2 = ptr2.next;
+            difference--;
+        }
+
+        while(ptr2.next != null) {
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+
+        ptr1.next = ptr1.next.next;
+
+        return dummy.next;
+    }
+}
